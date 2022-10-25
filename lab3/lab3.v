8ca0610375d32312a42b10d22d46a6d5e576f2fd
Emre Alçi <87760380+WualFabre@users.noreply.github.com>
2022-10-26 01:57:46 +0300

    Update lab3.v

--- lab3/lab3.v
+++ lab3/lab3.v
@@ -16,9 +16,9 @@ wire [10:0] e_result;
 parameter coef =6'b110010; 
 
 assign a_result = num1[6:5]; 
-assign b_result = {2{a_result}}; /
+assign b_result = {2{a_result}}; 
 assign c_result =  num1[2:0] ;
-assign d_result = num2[3:0]; //1110
+assign d_result = num2[3:0];
 assign e_result = {d_result,c_result,b_result}; 
 assign out1 = e_result / coef;
 
